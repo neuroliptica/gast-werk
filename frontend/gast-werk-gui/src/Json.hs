c90abd8c6af4aea6832c5bca1b3d92a7fdb4482b
Desu Desu <102465369+neuroliptica@users.noreply.github.com>
2023-04-17 22:16:36 +0300

    frontend: general errors + /api/get request

--- frontend/gast-werk-gui/src/Json.hs
+++ frontend/gast-werk-gui/src/Json.hs
@@ -2,19 +2,39 @@
 
 -- | Json.hs: provide json schemas.
 module Json
-  ( ApiGet(..)
+  ( GetResponse(..)
+  , Error(..)
   ) where
 
-import Data.Aeson
+import Data.Aeson hiding (Error)
 
--- | /api/get response.
-data ApiGet = ApiGet String String Int
+-- | General error body scheme.
+data ErrorBody = ErrorBody Bool String
     deriving Show
 
-instance FromJSON ApiGet where
+instance FromJSON ErrorBody where
+    parseJSON (Object v) = do
+        failed <- v .: "failed"
+        reason <- v .: "reason"
+        return (ErrorBody failed reason)
+
+-- | General error scheme.
+data Error = Error ErrorBody
+    deriving Show
+
+instance FromJSON Error where
+    parseJSON (Object v) = do
+        body <- v .: "error"
+        return (Error body)
+
+-- | /api/get response scheme.
+data GetResponse = GetResponse String String Int
+    deriving Show
+
+instance FromJSON GetResponse where
     parseJSON (Object v) = do
         base64 <- v .: "img_base64"
         hash <- v .: "hash"
         empty <- v .: "empty"
-        return (ApiGet base64 hash empty)
+        return (GetResponse base64 hash empty)
 
--- frontend/gast-werk-gui/src/Requests.hs
+++ frontend/gast-werk-gui/src/Requests.hs
@@ -5,13 +5,30 @@
 module Requests 
   ( jsonPost
   , jsonGet
+  , apiGet
   ) where
 
-import Data.Aeson
+import Data.Aeson hiding (Error)
 import Network.HTTP.Simple
 import Control.Logging as Log
 import Data.Text as T
 import Data.ByteString.Lazy as LBS
+
+import Json
+
+-- | Server body answer. Could be either a general error or a valid answer.
+type Answer l r = Maybe (Either l r)
+-- | Answer but inside IO monad.
+type AnswerIO l r = IO (Answer l r)
+
+-- | Generic decoder for api answers. Will return Nothing if both answer schemas failed.
+decoder :: (FromJSON l, FromJSON r) => LBS.ByteString -> Answer l r
+decoder answer = do
+    case decode answer :: FromJSON r => Maybe r of
+      Just valid -> return $ Right valid
+      Nothing -> do
+        failed <- decode answer :: FromJSON l => Maybe l
+        return $ Left failed
 
 -- | General HTTP POST with json request and response body.
 jsonPost :: ToJSON req => String -> req -> IO LBS.ByteString
@@ -23,8 +40,14 @@ jsonPost url body = Log.withStdoutLogging $ do
 
 -- | General HTTP GET with json response body.
 jsonGet :: String -> IO LBS.ByteString
-jsonGet url = do
+jsonGet url = Log.withStdoutLogging $ do
     Log.logS "GET" $ T.pack url
     response <- httpLBS <$> parseRequest url
     getResponseBody <$> response
+
+-- | HTTP GET to /api/get
+apiGet :: AnswerIO Error GetResponse
+apiGet = do
+    response <- jsonGet "http://localhost:8080/api/get"
+    return (decoder response :: Answer Error GetResponse)
 
