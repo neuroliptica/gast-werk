0fdaf7972f5e08b4183b01bf4944c101e5523f81
Desu Desu <102465369+neuroliptica@users.noreply.github.com>
2023-04-18 00:22:46 +0300

    frontend: general logger for gui and stdout

--- frontend/gast-werk-gui/src/Gui.hs
+++ frontend/gast-werk-gui/src/Gui.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards #-}
-{-# LANGUAGE NoMonomorphismRestriction          #-}
+-- {-# LANGUAGE NoMonomorphismRestriction          #-}
 
 -- | Gui.hs: main gui instance.
 module Gui
@@ -9,14 +9,43 @@ module Gui
 import GI.Gtk hiding (main, init)
 import GI.Gtk as Gtk (main, init)
 
+import Control.Logging as Log
+import Logger
+
 gui :: IO ()
 gui = do
     Gtk.init Nothing
     mainWindow <- windowNew WindowTypeToplevel
     windowSetTitle       mainWindow "gast-werk gui"
-    windowSetDefaultSize mainWindow 500 500
+    windowSetDefaultSize mainWindow 700 700
     windowSetPosition    mainWindow WindowPositionCenter
     onWidgetDestroy      mainWindow mainQuit
+
+    vPaned <- panedNew OrientationVertical
+    containerAdd mainWindow vPaned
+
+    topFrame <- frameNew Nothing
+    topFrameBox <- boxNew OrientationVertical 1
+
+    panedPack1 vPaned topFrame False False
+    containerAdd topFrame topFrameBox
+
+    button <- buttonNewWithLabel "Testing"
+    containerAdd topFrameBox button
+
+
+    logFrame <- frameNew Nothing
+    logFrameBox <- boxNew OrientationVertical 1
+
+    logBuffer <- (textBufferNew :: Maybe TextTagTable -> IO TextBuffer) Nothing
+    logView <- textViewNewWithBuffer logBuffer
+
+    onButtonClicked button $ do
+        logBoth logBuffer "button clicked"
+
+    panedPack2 vPaned logFrame False False
+    containerAdd logFrame logFrameBox
+    containerAdd logFrameBox logView
 
     widgetShowAll mainWindow
     Gtk.main
---
+++ frontend/gast-werk-gui/src/Logger.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Logger
+  ( logBoth
+  ) where
+
+import Control.Logging as Log
+import Data.Text as T
+import GI.Gtk
+import Data.ByteString.Char8 as B8
+import Data.Int
+
+-- | Insert Text in TextBuffer at TextIter position.
+textBufferInsertByteString :: TextBuffer -> TextIter -> T.Text -> IO ()
+textBufferInsertByteString buf pos msg = do
+    let len = fromIntegral (B8.length . B8.pack . T.unpack $ msg) :: Int32
+    textBufferInsert buf pos msg len
+
+-- | Write Text in Gtk.TextBuffer with MaxLines param specified.
+writeInBuffer :: TextBuffer -> T.Text -> Int32 -> IO ()
+writeInBuffer buffer msg maxlines = do
+    end <- textBufferGetEndIter buffer
+    textBufferInsertByteString buffer end msg
+    textBufferInsertByteString buffer end "\n"
+    len <- textBufferGetLineCount buffer
+    if (len > maxlines)
+    then do
+      start <- textBufferGetStartIter buffer
+      until <- textBufferGetIterAtLine buffer (len - maxlines)
+      textBufferDelete buffer start until
+    else return ()
+
+-- | Write message both in buffer and stdout.
+logBoth :: TextBuffer -> T.Text -> IO ()
+logBoth buf msg = Log.withStdoutLogging $ do
+    Log.log msg
+    writeInBuffer buf msg 10
